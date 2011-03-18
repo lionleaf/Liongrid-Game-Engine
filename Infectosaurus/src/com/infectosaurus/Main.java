393b242d4f6f87b0256d764e4fa62de0e07c8cab
Daniel Marelius Bjoernstad <neitakkikkebanan@gmail.com>
2011-03-18 11:17:22 +0000

    NewGame

---
+++ Infectosaurus/src/com/infectosaurus/Main.java
@@ -0,0 +1,16 @@
+package com.infectosaurus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new Panel(this));
+    }
+}
---
+++ Infectosaurus/src/com/infectosaurus/Panel.java
@@ -0,0 +1,75 @@
+package com.infectosaurus;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class Panel extends SurfaceView implements SurfaceHolder.Callback{
+	private InfectaThread mThread;
+	private int mX;
+	private int mY;
+	private Bitmap mBitmap;
+	private Bitmap fBitmap;
+	
+	public void doDraw(Canvas canvas) {
+	    canvas.drawBitmap(fBitmap, 0, 0, null);
+	    
+	    int fWidth = fBitmap.getWidth();
+	    int fHeight = fBitmap.getHeight();
+	    
+	    int tilesX = getWidth()/fWidth;
+	    int tilesY = getHeight()/fHeight;
+	    
+	    for(int i = 0; i < tilesX; i++){
+	    	for(int j = 0; j < tilesY; j++){
+	    		canvas.drawBitmap(fBitmap, i*fWidth + 1, j*fHeight + 1, null);
+	    	}
+	    }
+	    canvas.drawBitmap(mBitmap, mX, mY, null);
+	    canvas.drawCircle(getWidth()/2, getHeight()/2, 10, null);
+	}
+	
+	public Panel(Context context) {
+	    super(context);
+	    mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.icon);
+	    fBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.stonefloor);
+	    
+	    getHolder().addCallback(this);
+	    mThread = new InfectaThread(this);
+	}
+	 
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+	    if (!mThread.isAlive()) {
+	        mThread = new InfectaThread(this);
+	        mThread.setRunning(true);
+	        mThread.start();
+	    }
+	}
+	 
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+	    if (mThread.isAlive()) {
+	        mThread.setRunning(false);
+	    }
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
+	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mX = (int) event.getX() - mBitmap.getWidth() / 2;
+        mY = (int) event.getY() - mBitmap.getHeight() / 2;
+        return super.onTouchEvent(event);
+    }
+}
