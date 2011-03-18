878b5be3a1a258fb63da4a01434f2c9d5f308315
Daniel Marelius Bjoernstad <neitakkikkebanan@gmail.com>
2011-03-18 19:08:47 +0000

    The start of some delay in "game thread" infectathread

--- Infectosaurus/src/com/infectosaurus/InfectaThread.java
+++ Infectosaurus/src/com/infectosaurus/InfectaThread.java
@@ -6,6 +6,9 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 
 public class InfectaThread extends Thread {
+	/** Used to figure out elapsed time between frames */
+    private long mLastTime;
+    
     private Panel mPanel;
     private SurfaceHolder mHolder;
     private boolean mRun = false;
@@ -21,15 +24,24 @@ public class InfectaThread extends Thread {
  
     @Override
     public void run() {
-        Canvas canvas = null;
         while (mRun) {
-            canvas = mHolder.lockCanvas();
-            if (canvas != null) {
-                mPanel.doDraw(canvas);
-                mHolder.unlockCanvasAndPost(canvas);
-                
+        	Canvas canvas = null;
+        	try{
+	            canvas = mHolder.lockCanvas();
+	            if (canvas != null) {
+	            	mLastTime = System.currentTimeMillis() + 100;
+	                mPanel.doDraw(canvas);
+	            }
+        	}finally {
+                // do this in a finally so that if an exception is thrown
+                // during the above, we don't leave the Surface in an
+                // inconsistent state
+                if (canvas != null) {
+                    mHolder.unlockCanvasAndPost(canvas);
+                }
             }
         }
+        
     }
 
 	public void doTouchEvent(MotionEvent event) {
@@ -37,5 +49,18 @@ public class InfectaThread extends Thread {
 		if(infector.distance(mPanel.getWidth()/2, mPanel.getHeight()/2) < 10){
 			mPanel.color = Color.RED;
 		}
+	}
+	
+	private void updatePhysics(){
+		 long now = System.currentTimeMillis();
+
+         // Do nothing if mLastTime is in the future.
+         // This allows the game-start to delay the start of the physics
+         // by 100ms or whatever.
+         if (mLastTime > now) return;
+
+         double elapsed = (now - mLastTime) / 1000.0;
+         
+         mLastTime = now;
 	}
 }
