4d1abfea75c3865e1ae6a388caff0a021067b568
Daniel Marelius Bjoernstad <neitakkikkebanan@gmail.com>
2011-04-19 15:07:53 +0000

    Needs velocity

--- Infectosaurus/src/com/infectosaurus/components/BehaviorComponent.java
+++ Infectosaurus/src/com/infectosaurus/components/BehaviorComponent.java
@@ -12,40 +12,60 @@ import com.infectosaurus.crowd.behaviorfunctions.BehaviorFunction;
  * The probability of each of the states are modified by the behavior
  * functions. 
  */
-public class CrowdComponent extends Component{
+public class BehaviorComponent extends Component{
 	
+
+	public static final int DEFAULT_STATES = 5;
 	private final static int MAX_BEHAVIOURS = 32;
 	FixedSizeArray<BehaviorFunction> behaviours = 
 		new FixedSizeArray<BehaviorFunction>(MAX_BEHAVIOURS);
+	float[] defaultProb = new float[MAX_BEHAVIOURS]; 
+	State[] defaultStates = new State[DEFAULT_STATES];
+	State curState;
+	
+	public BehaviorComponent() {
+		for (int i = 0; i < defaultStates.length; i++) {
+			defaultStates[i] = new State();
+			defaultStates[i].turnAngle = Math.PI/2 - (i*Math.PI/4);
+		}
+		
+		for (int i = 0; i < defaultStates.length; i++) {
+			for (int j = 0; j < defaultStates.length; j++) {
+				defaultStates[i].addNextState(defaultStates[j]);
+			}
+		}
+		
+		curState = defaultStates[2];
+	}
 	
 	@Override
 	public void update(float dt, BaseObject parent) {
 		checkSituationChange();		
 		
-		State[] states = calculateStates();
-		float[] defaultProb = calculateDefaultProb(states);
+		calculateDefaultProb();
 		
 		Object[] bObjects = behaviours.getArray();
 		int size = behaviours.getCount();
 		for (int i = 0; i < size; i++) {
 			BehaviorFunction bf = (BehaviorFunction) bObjects[i];
-			bf.update(states, defaultProb);
+			bf.update((State[]) curState.nextStates.getArray(), defaultProb);
 		}
 		
 	}
 
 	private void checkSituationChange() {
-		// TODO Auto-generated method stub
+	}
+
+	private void calculateDefaultProb() {
+		for(int i = 0;  i < defaultProb.length; i++){
+			if(i < curState.nextStates.getCount()) 
+				defaultProb[i] = 1.0f/curState.nextStates.getCount();
+			else defaultProb[i] = 0f;
+		}
+	}
+
+	private void calculateStates() {
 		
-	}
-
-	private float[] calculateDefaultProb(State[] states) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private State[] calculateStates() {
-		return null;
 	}
 
 }
--- Infectosaurus/src/com/infectosaurus/crowd/State.java
+++ Infectosaurus/src/com/infectosaurus/crowd/State.java
@@ -20,8 +20,10 @@ public class State extends BaseObject {
 	float time;
 	
 	//Action action;  Create the class.
-	
+	static final int MAX_STATES = 5;
 	FixedSizeArray<State> previousStates;
+	public FixedSizeArray<State> nextStates = new FixedSizeArray<State>(MAX_STATES);
+	public double turnAngle = 0;
 	
 	public State(){ // Has to have this if we want it pooled
 		
@@ -36,7 +38,14 @@ public class State extends BaseObject {
 		//Action.reset()
 		
 	}
+
+	public void addNextState(State state) {
+		nextStates.add(state);
+	}
 	
+	public void removeState(State state){
+		nextStates.remove(state, true);
+	}
 	
 	
 }
