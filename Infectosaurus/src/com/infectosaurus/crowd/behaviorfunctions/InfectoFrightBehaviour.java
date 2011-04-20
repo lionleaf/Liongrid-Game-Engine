7e6d8261ac55ccc9ffc1b3094d78b2dac65c59ca
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2011-04-20 13:44:20 +0000

    Fright sortof works a tiny bit better now.

--- Infectosaurus/src/com/infectosaurus/crowd/behaviorfunctions/InfectoFrightBehaviour.java
+++ Infectosaurus/src/com/infectosaurus/crowd/behaviorfunctions/InfectoFrightBehaviour.java
@@ -1,21 +1,33 @@
 package com.infectosaurus.crowd.behaviorfunctions;
 
 import com.infectosaurus.Infectosaurus;
+import com.infectosaurus.Vector2;
 import com.infectosaurus.crowd.State;
 
 public class InfectoFrightBehaviour extends BehaviorFunction {
 	Infectosaurus inf = null;
 	static final float nr = (float) ((Math.PI*Math.PI)/8);
+	Vector2 vec = new Vector2();
+	
 	@Override
 	protected float evaluate(State s, State lastState) {
 		inf = gamePointers.currentSaurus;
 		if(inf == null) return 1;
 		
-		float cDist = s.pos.distance2(inf.pos);
-		float lastDist = lastState.pos.distance2(inf.pos);
-		float dtPos = lastState.pos.distance2(s.pos);
-		float dtDist = (cDist - lastDist); //Negative if we`re getting closer
-		return (10*dtDist/dtPos); //Returns 10 if all distance was moved away
+		vec.set(inf.pos);
+		vec.subtract(lastState.pos);
+		float angleToInfect = vec.getAngle();
+		float lastAngle = lastState.vel.getAngle() - angleToInfect ;
+		float newAngle = s.vel.getAngle() - angleToInfect ;
+		
+		if((newAngle*newAngle) >= (lastAngle*lastAngle)){
+			return 10;
+		}else{
+			return -10; 
+		}
+		
+		
+		
 	}
 
 	@Override
