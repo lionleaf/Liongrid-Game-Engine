b176e99eb15fb6f02bec251b4caa35bee80893d2
Daniel Marelius Bjoernstad <neitakkikkebanan@gmail.com>
2011-05-05 00:04:32 +0000

    Added some comment

--- Infectosaurus/src/com/infectosaurus/crowd/situations/Situation.java
+++ Infectosaurus/src/com/infectosaurus/crowd/situations/Situation.java
@@ -7,6 +7,11 @@ public abstract class Situation {
 		applySituation(go);
 	}
 
+	/**
+	 * The method can put in and out components, add and remove components
+	 * and add or remove states. 
+	 * @param go GameObject the situation should apply its modifiers to
+	 */
 	abstract void applySituation(GameObject go);
 		
 }
