b8cc613ac5840ef3e9bd61d7c4ebd207ad551a7a
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2011-05-31 14:50:25 +0000

    Added TextureLibrary, but it is not used yet.

--- Infectosaurus/src/com/liongrid/gameengine/Texture.java
+++ Infectosaurus/src/com/liongrid/gameengine/Texture.java
@@ -1,5 +1,11 @@
 package com.liongrid.gameengine;
 
+/**
+ * Simple container class for textures.  Serves as a mapping between Android resource ids and
+ * OpenGL texture names, and also as a placeholder object for textures that may or may not have
+ * been loaded into vram.  Objects can cache Texture objects but should *never* cache the texture
+ * name itself, as it may change at any time.
+ */
 public class Texture {
 	public int resource;
     public int name;
