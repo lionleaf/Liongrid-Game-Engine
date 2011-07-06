0aa4e660e60ed58b2f6de8092dd55e150a4e1517
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2011-07-06 01:36:13 +0000

    added reset stats button

--- Infectosaurus/src/com/liongrid/infectosaurus/Main.java
+++ Infectosaurus/src/com/liongrid/infectosaurus/Main.java
@@ -2,6 +2,8 @@ package com.liongrid.infectosaurus;
 
 
 
+import com.liongrid.infectosaurus.upgrades.InfectosaurusUpgrade;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -75,6 +77,25 @@ public class Main extends Activity {
         	
         });
         
+        
+        View resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				resetProgress();
+				
+			}
+		});
+    }
+    
+    private void resetProgress(){
+    	InfectoPointers.coins = 0;
+    	InfectosaurusUpgrade[] upgrades = InfectosaurusUpgrade.values();
+    	for (int i = 0; i < upgrades.length; i++) {
+			upgrades[i].get().setRank(0);
+    	}
+    	
+    	GameActivity.saveData(getApplicationContext());
     }
     
     @Override
