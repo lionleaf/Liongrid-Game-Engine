7bbfc7d14b255d0c342c4952fd1cd26fa902910c
Daniel Marelius Bjoernstad <neitakkikkebanan@gmail.com>
2011-07-14 15:58:50 +0000

    EasyBitmapCropper should now work. Tested it a bit.

--- Infectosaurus/src/com/liongrid/gameengine/EasyBitmapCropper.java
+++ Infectosaurus/src/com/liongrid/gameengine/EasyBitmapCropper.java
@@ -16,25 +16,36 @@ public class EasyBitmapCropper {
 		return cropWorkspace;
 	}
 	
-	public static int[]	moveCrop(Texture tex, int[] cropWorkspace, int index){
-		int offsetX = cropWorkspace[0];
-		while(offsetX > tex.width) offsetX -= tex.width;
-		int offsetY = tex.height - cropWorkspace[1];
-		while(offsetY > tex.height) offsetY -= tex.height;
+	public static int[]	moveCrop(Texture tex, int[] cropWorkspace, int squareX, int squareY){
+		int x = cropWorkspace[0];
+		while(x > tex.width) x -= tex.width;
+		int y = tex.height - cropWorkspace[1];
+		while(y > tex.height) y -= tex.height;
 		
 		int cropWidth = cropWorkspace[2];
 		int cropHeight = - cropWorkspace[3];
 		
-		int lengthX = (tex.width - offsetX)%cropWidth;
-		int lengthY = (tex.height - offsetY)%cropHeight;
+		int[] newCropWorkspace = new int[4];
+		newCropWorkspace[0] = x + squareX*cropWidth;
+		newCropWorkspace[1] = tex.height - (y + squareY*cropHeight);
+		newCropWorkspace[2] = cropWorkspace[2];
+		newCropWorkspace[3] = cropWorkspace[3];
+		return newCropWorkspace;
+	}
+	
+	public static int[]	moveCrop(Texture tex, int[] cropWorkspace, int squareX, int squareY,
+			int offsetX, int offsetY){
+		int x = cropWorkspace[0];
+		while(x > tex.width) x -= tex.width;
+		int y = tex.height - cropWorkspace[1];
+		while(y > tex.height) y -= tex.height;
 		
-		int j = index%lengthX;
-		int i = index - j*lengthX;
-		if(i >= lengthX || j >= lengthY) return null;
+		int cropWidth = cropWorkspace[2];
+		int cropHeight = - cropWorkspace[3];
 		
 		int[] newCropWorkspace = new int[4];
-		newCropWorkspace[0] = offsetX + i*tex.width;
-		newCropWorkspace[1] = tex.height - (offsetY + j*tex.height);
+		newCropWorkspace[0] = x + squareX*(cropWidth + offsetX);
+		newCropWorkspace[1] = tex.height - (y + squareY*(cropHeight + offsetY));
 		newCropWorkspace[2] = cropWorkspace[2];
 		newCropWorkspace[3] = cropWorkspace[3];
 		return newCropWorkspace;
